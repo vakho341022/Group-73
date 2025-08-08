59a8ed387a1b46e5d5339be93b202393e425412a
vakho341022 <vakhochkhikvadze1@gmail.com>
2025-08-08 19:19:48 +0400

    Add files via upload

---
+++ day 005/classwork/lvl 05.py
@@ -0,0 +1,54 @@
+# debugging ეწოდება შეცდომების გასწორებას
+# case insensitive ეწოდება, როდესაც დიდი მნიშვნელობა აქვს პროგრამირებისას დაწერილ დიდ და პატარა ასოებს
+# python არის case insensitive, რადგან ბრძანების დაწერისას მნიშვნელოვანია დიდი და პატარა ასოებზე დაკვირვება.
+
+mobile_series = 16
+team_name = "rainbow"
+math_answer = 10000000001
+book_name = "white fang"
+app_count = 15
+print(mobile_series, team_name, math_answer, book_name, app_count)
+print(mobile_series)
+print(team_name)
+print(math_answer)
+print(book_name)
+print(app_count)
+
+# # it needs (=) instead of (->)
+# mobile_series -> 16
+# # missing second quotation mark
+# team_name = "rainbow" \
+# # don't need quotation mark at the end
+# math_answer = 10000000001""
+# # it needs (=) instead of (->) and missing quotation mark
+# book_name -> "white fang
+# # it needs undersqore(_) instead (+)
+# app+count = 15
+
+
+num1 = 100
+num2 = 1000
+num3 = num1 + num2
+
+num4 = 1000000
+num5 = 999999
+num6 = num4 - num5
+
+num7 = 999999
+num8 = 1
+num9 = num7 * num8
+
+num10 = 193
+num20 = 3
+num30 = num10 / num20
+
+num34 = 144
+num24 = 12
+num35 = num34 // num24
+
+print(num3, num6, num9, num30, num35)
+print(num3)
+print(num6)
+print(num9)
+print(num30)
+print(num35)
---
+++ day 005/homework/lvl 05.py
@@ -0,0 +1,68 @@
+# დავალება №1 იყო sololearn-ში
+
+# დავალება №2
+# კომენტარები გამოიყენება ადამიანებისთვის რათა გრძელ კოდში ადვილად იყოს შესაძლებელი თითოეული ნაბიჯის გაგება.(კომპიუტერი კომენტარებს ვერ კითხულობს)
+
+# დავალება №3
+person_name = "vakho"
+person_surname = "chkhikvadze"
+person_age = 16
+birth_day = 10
+birth_month = "july"
+birth_year = 2009
+person_mail = "vakhochkhikvadze1@gmail.com"
+phone_number = "5951****0"
+
+print(person_name)
+print(person_surname)
+print(person_age)
+print(birth_day, birth_month, birth_year)
+print(person_mail)
+print(phone_number)
+
+# დავალება №4
+# BEFORE
+# 1name = " Luka"
+#   age num => 56
+#   my!favmov = blood and bone
+#   my_friend's_name -> Nika
+  
+#   print("My name is" + 1name)
+#   print("my age is" + age num)
+#   print("My Favorite Movie is" + my!favmov)
+#   print("My Friend's name is" + my_friend's_name)
+
+#AFTER
+name1 = "Luka"
+age_num = 56
+my_favmov = "blood and bone"
+my_friend_name = "Nika"
+
+print("My name is",  name1)
+print("my age is", age_num)
+print("My Favorite Movie is" + my_favmov)
+print("My Friend's name is" + my_friend_name)
+
+# დავალება №5
+
+name = "vakho"
+surname = "chkhikvadze"
+print("name and surname:" + name + "," + surname)
+
+snake_name = "cobra"
+egg_form = "circle"
+print("snake information:" + snake_name + "," + egg_form)
+
+color1 = "red"
+color2 = "blue"
+print("purple =" + color1 + "+" + color2)
+
+computer_type = "laptop"
+computer_series = "HP"
+print("computer model:" + computer_type + "," + computer_series)
+
+program1 = "java"
+program2 = "python"
+print(program1 + "/" + "VS" + "/" + program2)
+
+# snake_case is variable type which is written with underscore( _ )
