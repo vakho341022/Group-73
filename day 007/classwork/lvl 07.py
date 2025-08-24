a08d78aa782f51cac349f4d73ed1612974c8d3d7
vakho341022 <vakhochkhikvadze1@gmail.com>
2025-08-24 21:19:31 +0400

    Add files via upload

---
+++ day 007/classwork/lvl 07.py
@@ -0,0 +1,61 @@
+# task 1
+
+# input - შემეველი ინფორმაცია 
+# output - გამომავალი ინფორმაცია 
+
+# task 2 
+
+# კონკატინაცია - ორი სტრინგის შეერთება
+
+# task 3
+
+email = input("write your email: ")
+print(email)
+
+# task 4
+
+name = input("your name: ")
+surname = input("your surname: ")
+age = input("your age: ")
+adress = input("your adress: ")
+favourite_color = input("your favourite color: ")
+favourite_car = input("your favourite car: ")
+print("hello my name is " + name) 
+print("my surname is " + surname)
+print("I am " + age + "years old")
+print("I live in " + adress)
+print("my favourite car and color are " + favourite_car + "," + favourite_color)
+
+# task 5
+
+num1 = input("write num1: ")
+num2 = input("write num2: ")
+age = input("write your age: ")
+number = input("write number: ")
+num6 = input("write num6: ")
+
+int(num1)
+int(num2)
+int(age)
+int(number)
+int(num6)
+
+# task 6
+
+num7 = int(input("write num1: "))
+num8 = int(input("write num2: "))
+age9 = int(input("write your age: "))
+number0 = int(input("write number: "))
+num66 = int(input("write num6: "))
+
+str(num7)
+str(num8)
+str(age9)
+str(number0)
+str(num66)
+
+# task 7
+
+numbers = input("write any number: ")
+int(numbers)
+print(numbers + 15)
---
+++ day 007/homework/lvl 07.py
@@ -0,0 +1,38 @@
+# task 1
+
+# დავალება იყო sololearn-ში
+
+# task 2
+
+# input - შემეველი ინფორმაცია 
+# output - გამომავალი ინფორმაცია 
+
+# task 3 
+
+# კონკატინაცია - ორი სტრინგის შეერთება
+
+# task 4
+
+# str = "10"
+# int = 10
+
+# print(str + int)
+# print(str - int)
+# print(str * int)
+# print(str / int)
+
+# task 5
+
+height = input("write height: ")
+float(height)
+print(type(height))
+
+# task 6
+
+# data convertion - არის მონაცემების ტიპის შეცვლა
+
+# task 7
+
+# implicitly - როდესაც პითონი ავტომატურად ცვლის მონაცემთა ტიპს
+# explicitly - როდესაც პროგრამისტი ცვლის მონაცემთა ტიპს
+# მონაცემთა ტიპს ცვლიან შემდეგი ფუნქციები: int(), str(), float().
