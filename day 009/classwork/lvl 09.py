0646079c25232bfe01c49df088a74754efd408f1
vakho341022 <vakhochkhikvadze1@gmail.com>
2025-09-13 22:21:57 +0400

    Add files via upload

---
+++ day 009/classwork/lvl 09.py
@@ -0,0 +1,52 @@
+# task 1
+
+# logical operators - ოპერატორები რომლებიც ამოწმებენ პირობებს.
+# and – აბრუნებს True მხოლოდ მაშინ, როცა ორივე პირობა სიმართლეა.
+# or – აბრუნებს True, თუ რომელიმე პირობა მაინც სიმართლეა.
+
+# true and true = true
+# true and false = false
+# false and true = false
+# false and false = false
+
+# true or true = true
+# true or false = true
+# false or true = true
+# false or false = false
+
+# task 2
+
+name = input("write your name: ")
+print(name == "ixvi")
+
+# task 3
+
+number = int(input("write your age: "))
+print(number > 50)
+
+# task 4
+
+favourite_toy = input("write your favourite toy: ")
+my_favourite_toy = "car"
+print(favourite_toy == my_favourite_toy)
+
+# task 5
+
+# it will print (true)
+
+# task 6
+
+num1 = 4
+num2 = 100000000
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 == num2)
+print(num1 < num2)
+print(num1 > num2)
+
+# task 7
+
+age = 10
+name = "saba"
+print(age == 10 and name == "saba")
+print(age == 10 or name == "davit")
---
+++ day 009/homework/lvl 09.py
@@ -0,0 +1,59 @@
+# task 1
+
+# task was in sololearn
+
+# task 2
+
+num1 = 29
+num2 = 999999999
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 == num2)
+print(num1 < num2)
+print(num1 > num2)
+
+# task 3
+
+# logical operators - ოპერატორები რომლებიც ამოწმებენ პირობებს.
+# and – აბრუნებს True მხოლოდ მაშინ, როცა ორივე პირობა სიმართლეა.
+# or – აბრუნებს True, თუ რომელიმე პირობა მაინც სიმართლეა.
+
+# true and true = true
+# true and false = false
+# false and true = false
+# false and false = false
+
+# true or true = true
+# true or false = true
+# false or true = true
+# false or false = false
+
+# task 4
+
+age = 20
+name = "vano"
+number1 = 10
+print(age == 15 and name == "vano")
+print(number1 == 20 and name == "vakho")
+print(age == 20 and name == "vano")
+
+print(age == 20 or name == "vakho")
+print(number1 == 15 or name == "vano")
+print(age == 20 or name == "vano")
+
+# task 5
+
+my_num = 10
+number = int(input("write your num: "))
+print(number > my_num)
+
+# task 6
+
+favourite_name = input("write your favourite car: ")
+my_favourite_name = "Dato"
+print(favourite_name == my_favourite_name)
+
+# task 7
+
+number = int(input("write your age: "))
+print(number > 18)
